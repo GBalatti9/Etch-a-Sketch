4b13a6bd5a0bc1b901f51c08ea120e1b452c7c12
GBalatti9 <gas.balatti@gmail.com>
2023-08-06 16:38:27 -0300

    Buttons Pick color button, Eraser button and Clear button working

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 window.onload = () => {
 
     const container = document.querySelector('.container');
-    console.log(container);
     const createGridPx = () => {
-        const numberOfPx = 1024;
-        const containerWidth = 500; // Width of the container in pixels
-        const pixelsPerRow = 32; // Number of pixels to display in each row
-        const pixelSize = containerWidth / pixelsPerRow; // Calculate the size of each pixel
 
-        for (let i = 0; i <= numberOfPx; i++) {
+        for (let i = 0; i <= 1024; i++) {
             const pixel = document.createElement('div');
             pixel.classList.add('pixel');
-            pixel.style.width = `${pixelSize}px`;
-            pixel.style.height = `${pixelSize}px`;
-            // pixel.style.border = '.5px solid black';
-
             container.appendChild(pixel);
         }
     }
     createGridPx();
 
     const pixelDOM = Array.from(document.querySelectorAll('.pixel'));
+    const chooseColor = document.getElementById('color');
+    const eraser = document.getElementById('eraser');
+    const clear = document.getElementById('clear');
     pixelDOM.forEach(pixel => {
         pixel.addEventListener('mouseover', () => {
-            pixel.style.backgroundColor = 'red';
+            pixel.style.backgroundColor = chooseColor.value;
         })
     })
 
-    const px = document.querySelector('#px');
-    px.addEventListener('click', () => {
+    eraser.addEventListener('click', () => {
+        chooseColor.value = '#ffffff'
+        pixelDOM.forEach(pixel => {
+            pixel.addEventListener('mouseover', () => {
+                pixel.style.backgroundColor = 'white';
+            })
+        })
+    })
+
+    
+    clear.addEventListener('click', () => {
+        pixelDOM.forEach(pixel => {
+            pixel.style.backgroundColor = 'white'
+        })
+    })
+
+    const changeSizePx = document.querySelector('#px');
+    changeSizePx.addEventListener('click', () => {
         pixelDOM.forEach(pixel => {
             pixel.style.backgroundColor = 'white'
             pixel.style.width = '50px'
@@ -36,4 +46,6 @@ window.onload = () => {
         })
     })
 
+
+    console.log(color.value);
 }
