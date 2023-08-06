42017e32b5f934a66a758695e9d20740a9515d25
GBalatti9 <gas.balatti@gmail.com>
2023-08-06 18:22:11 -0300

    Rainbow logic The application has a rainbow button which function is to paint with random color each pixel of the grid

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ window.onload = () => {
         chooseColor.value = '#ffffff'
         pixelDOM.forEach(pixel => {
             pixel.addEventListener('mouseover', () => {
-                pixel.style.backgroundColor = 'white';
+                pixel.style.backgroundColor = 'rgb(255, 255, 255)';
             })
         })
     })
@@ -46,9 +46,19 @@ window.onload = () => {
 
     const changeSizePx = document.getElementById('number-px');
     // console.log(changeSizePx);
+
+    changeSizePx.addEventListener('click', (e) => {
+        console.log(e);
+        changeSizePx.style.backgroundColor = 'white';
+    })
+
     changeSizePx.addEventListener('input', (e) => {
         let pxs = e.target.value
-
+        if(pxs.length > 0){
+            changeSizePx.style.backgroundColor = 'white';
+        } else{
+            changeSizePx.style.backgroundColor = 'rgb(84, 84, 84)';
+        }
         pixelDOM.forEach(pixel => {
             if (pxs >= 16 && pxs <= 100) {
                 pixel.style.width = `${pxs}px`;
@@ -57,5 +67,56 @@ window.onload = () => {
         })
     })
 
+    const gridState = document.getElementById('grid-state');
+    const label = document.querySelector('label[for="grid-state"]');
+    label.textContent = 'Turn grid off'
+
+    
+    const toggleGridState = () => {
+        if(gridState.checked){
+            label.textContent = 'Turn grid on';
+            pixelDOM.forEach(pixel => {
+                pixel.style.border = 'none';
+            })
+        } else {
+            label.textContent = 'Turn grid off';
+            pixelDOM.forEach(pixel => {
+            pixel.style.border = '1px solid black';
+            });
+        }
+    }
+    gridState.addEventListener('change', toggleGridState)
+
     console.log(color.value);
+
+    const rainbow = document.getElementById('rainbow');
+    
+    const pickRandomColor = () => {
+        let random = {
+            firstNumber: Math.floor(Math.random() * (256 - 0) - 0),
+            secondNumber: Math.floor(Math.random() * (256 - 0) - 0),
+            thirdNumber: Math.floor(Math.random() * (256 - 0) - 0),
+        };
+        let randomColor = `rgb(${random.firstNumber}, ${random.secondNumber}, ${random.thirdNumber})`
+        return randomColor;
+    }
+
+    rainbow.addEventListener('click', () => {
+        chooseColor.value = '#000000';
+        pixelDOM.forEach(pixel => {
+            pixel.addEventListener('mouseover', () => {
+                pixel.style.backgroundColor = pickRandomColor();
+            })
+        })
+    });
+
+    // const crazy = document.getElementById('crazy');
+    // crazy.addEventListener('click', () => {
+    //     chooseColor.value = '#000000';
+    //     container.addEventListener('mouseover', () => {
+    //         pixelDOM.forEach(pixel => {
+    //             pixel.style.backgroundColor = pickRandomColor();
+    //         })
+    //     })
+    // })
 }
